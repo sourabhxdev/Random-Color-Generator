eb026d5baf70e882159ff63ee87bcc08a9bf2a4c
Sourabh <sourabhxdev@gmail.com>
2025-03-27 15:34:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 var btn = document.querySelector('button')
 var box = document.querySelector('.box')
 var mess = document.querySelector('.message')
-let r;
-let g;
-let b;
+
 function changeColor() {
-    r = Math.floor(Math.random() * 256)
-    g = Math.floor(Math.random() * 256)
-    b = Math.floor(Math.random() * 256)
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
     
     box.style.backgroundColor = `rgb(${r},${g},${b})`
     mess.innerHTML = `rgb(${r},${g},${b})`
